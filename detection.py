6cef3cba028b05d4961aee3a83c860fb1a772a93
Tsugawa007 <59519629+Tsugawa007@users.noreply.github.com>
2020-12-26 01:36:34 +0900

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,28 +1,29 @@
 import jetson.inference
 import jetson.utils
 import cv2
+import itertools
+
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson.utils.gstCamera(1280, 720, "0")
+camera = jetson.utils.gstCamera(1280, 720, "/dev/video1")
 display = jetson.utils.glDisplay()
 
 try:
-    i = 0
     while display.IsOpen:
         img, width, height = camera.CaptureRGBA()
         detections = net.Detect(img, width, height)
         display.RenderOnce(img, width, height)
-        #print(detections)
-        print(i)
-        '''
-        for detection in detections:
-            if detection.ClassID == 1:
-                print(detection)
+        human_data = list()
         
-        dist_matrix = [ [ minkowski([detecions[i].Center[0], detections[i].Center[1]], [detecions[j].Center[0], detections[j].Center[1] ], 1) for j in range(i, len(detections))] for i in range(len(detections)) ] 
-        '''
-        dist_matrix = [ minkowski([detecions[0].Center[0], detections[i].Center[1]], [detecions[j].Center[0], detections[j].Center[1] ], 1) ] 
-        print()
-        i += 1
+        for detection in detections:
+            if detection.ClassID == 1 and detecton.Confidence >= 0.60:
+                human_data.append(detection)
+         
+        if len(human_data) >= 2:
+            for a,b in list(itertools.combination(human_data,2)):
+                if (abs(a.Center[0] - b.Center[0]) <= (a.Width /2 + b.Width / 2)):
+                    print("Yes")
+                else:
+                    print("No")
         display.SetTitle("Objet Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
         #display.close()
 except KeyboardInterrupt:
